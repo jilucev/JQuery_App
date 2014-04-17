8fa192da9cb4652e9f904eb03fd14a2d762cf9ec
jilucev <jilucev7@gmail.com>
2014-04-17 23:44:07 +0000

    commit

--- application.js
+++ application.js
@@ -1,12 +1,32 @@
 $ (function() {
-  $("#submit-button").click(function() {
+
+  // $("#submit-button").click(function() {
+    
+  // });
+
+  function addContactToPage() {
     $ul = $('<ul></ul>')
-    $('.page').append($ul)
+    $('.page').append($ul);
+
     $("#new-contact-form input").each(function () {
       var $this = $(this)
-      
-      $ul.append($('<li>' + $this.val() + '</li>'));
-        
+      $ul.append($('<li>' + $this.val() + '</li>'));       
     });
+  }
+
+  $('#new-contact-button').click(function(){
+    var $form = $('#new-contact-form')
+    $form.fadeIn();
+  });
+
+  $('body').on('click', '#submit-button', function(){
+    var firstname = $('#firstname').val();
+    
+    if (firstname === "") {
+      alert("firstname can't be blank");
+    } else {
+      addContactToPage();
+    }
   });
 });
+
